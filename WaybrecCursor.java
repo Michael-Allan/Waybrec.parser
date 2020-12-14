7bab14d216de5ac625c42012329e3e291b3d0e4c
Michael Allan <mike@reluk.ca>
2020-12-14 06:43:20 -0500

    Nominal clarity. Reusable parser.

---
+++ WaybrecCursor.java
@@ -0,0 +1,12 @@
+package wayic.Waybrec.parser;
+
+import Breccia.parser.BrecciaCursor;
+import java.nio.file.Path;
+
+
+/** A cursor-based pull parser of Waybrec.
+  */
+public class WaybrecCursor extends BrecciaCursor {}
+
+
+                                                        // Copyright © 2020  Michael Allan.  Licence MIT.
---
+++ WaybrecXCursor.java
@@ -0,0 +1,13 @@
+package wayic.Waybrec.parser;
+
+import Breccia.parser.*;
+
+
+public class WaybrecXCursor extends BrecciaXCursor {
+
+
+    public <S extends BreccianCursor & ReusableCursor> WaybrecXCursor( S sourceCursor ) {
+        super( sourceCursor ); }}
+
+
+                                                        // Copyright © 2020  Michael Allan.  Licence MIT.
