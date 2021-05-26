dc6e9116fcc00d4ba6e2016df9fee9f4ec0ceb0a
Michael Allan <mike@reluk.ca>
2021-05-26 17:51:21 -0400

    Sync with Waybrec.

--- WaybrecCursor.java
+++ WaybrecCursor.java
@@ -10,7 +10,6 @@ public class WaybrecCursor extends BrecciaCursor {
 
 
     public WaybrecCursor() {
-        resourceIndicantQualifiers.add( "uninterned" );
         final String[] commandPointKeywords = { // Those specific to Waybrec, in lexicographic order.
             "thoroughfractum" };
         final CommandPoint_<?>[] commandPoints = { // Each at the same index as its keyword above.
